35907798f3ac2f1000fb1ed14328a03f0a394413
appleowner <alpuente@crimson.ua.edu>
2016-01-16 12:30:22 -0600

    added display of purchased items:

--- app/src/main/java/com/example/appleowner/spendingtracker/MainActivity.java
+++ app/src/main/java/com/example/appleowner/spendingtracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appleowner.spendingtracker;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static SQLiteDatabase db;
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         Context context = this;
 
+        TextView textView = (TextView) findViewById(R.id.spending); // get the text view
+        textView.setText(getSpending());
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +43,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        TextView textView = (TextView) findViewById(R.id.spending); // get the text view
+        textView.setText(getSpending());
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public String getSpending() {
+        ArrayList<String> values = new ArrayList<String>();
+        String output = "";
+        Cursor c = MainActivity.db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        c.moveToFirst();
+        String[] items = new String[c.getCount()-1];
+        c.moveToNext(); // skip android_metadata table"insert
+        int i = 0;
+        while (c.isAfterLast() == false) {
+            System.out.println(c.getString(c.getColumnIndex("name")));
+            items[i] = c.getString(c.getColumnIndex("name"));
+            c.moveToNext();
+            i++;
+        }
+
+        int total = 0;
+        for (i = 0; i < items.length; i++) {
+            int interimTotal = 0;
+            Cursor curs = db.rawQuery("select * from " + items[i] + ";", null);
+            curs.moveToFirst();
+            curs.moveToNext();
+            System.out.println("curs length" + curs.getCount());
+            while (curs.isAfterLast() == false) {
+                System.out.println(curs.toString());
+                System.out.println("price: " + curs.getString( curs.getColumnIndex("price")));
+                int price = curs.getInt(curs.getColumnIndex("price"));
+                System.out.println(price);
+                interimTotal += price;
+                total += price;
+                curs.moveToNext();
+            }
+            output += items[i] + ": " + interimTotal + "\n";
+        }
+
+        output += "Total Spent: " + total;
+
+        return output;
     }
 
     @Override
